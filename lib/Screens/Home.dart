1f4f603e236bd4b71944df6a50de1a3d0f418701
imran <imkhan1070009@gmail.com>
2021-04-10 00:42:53 +0530

    Home page structure

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:credilio_news/CommonScreens/AppBarCommon.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -51,7 +52,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       key: scaffoldKey,
-      extendBodyBehindAppBar: true,
+      // extendBodyBehindAppBar: true,
       appBar: new AppBarCommon(
         title: Text(
           "CREDILIO",
@@ -72,10 +73,115 @@ class _HomeState extends State<Home> {
       body: WillPopScope(
           onWillPop: _onBackPressed,
           child: Container(
-            child: Center(
-              child: Text("HOME"),
-            ),
+            child: getNews(),
           )),
+    );
+  }
+
+  getNews() {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15, top: 0),
+            child: Text(
+              "Breaking News",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  letterSpacing: -1,
+                  color: Colors.black,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.normal),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 10),
+            child: SizedBox(
+              height: 450,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 30,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        // elevation: 2,
+                        // color: Colors.transparent,
+                        child: Container(
+                          decoration: new BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          height: 400,
+                          width: 300,
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ),
+          StickyHeader(
+            header: Container(
+              color: Colors.white,
+              height: 50,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 7,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding:
+                          const EdgeInsets.only(left: 15, bottom: 10, top: 10),
+                      child: Container(
+                        child: Text(
+                          "Category$index",
+                          style: TextStyle(
+                              letterSpacing: -1,
+                              color: Colors.grey[300],
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            content: Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 30,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      // elevation: 2,
+                      // color: Colors.transparent,
+                      child: Container(
+                        decoration: new BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: 100,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    );
+                  }),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
